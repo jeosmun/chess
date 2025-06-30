286e2defd961655c9b87fbda775c14a733616b3b
Jacob Edward Osmun <osmunjacob@gmail.com>
2025-06-30 15:40:35 -0600

    BishopMovesCalculator now passes the first test. Added validMove() to the PieceMovesCalculator to be used in successive calculators. Only checks to see if the piece is still on the board for now.

--- shared/src/main/java/chess/BishopMovesCalculator.java
+++ shared/src/main/java/chess/BishopMovesCalculator.java
@@ -4,10 +4,39 @@ import java.util.Collection;
 
 class BishopMovesCalculator extends PieceMovesCalculator {
 
-    public BishopMovesCalculator() {};
+    public BishopMovesCalculator() {}
 
     @Override
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        return new java.util.ArrayList<>();
-    };
+        Collection<ChessMove> moves = new java.util.ArrayList<>();
+        // Check up and to the left
+        ChessPosition newPosition = new ChessPosition(myPosition.getRow(), myPosition.getColumn());
+        newPosition.updatePosition(1, -1);
+        while(this.validMove(newPosition)) {
+            moves.add(new ChessMove(myPosition, newPosition.copy(), null));
+            newPosition.updatePosition(1, -1);
+        }
+        // Check up and to the right
+        newPosition.updatePosition(myPosition);
+        newPosition.updatePosition(1, 1);
+        while(this.validMove(newPosition)) {
+            moves.add(new ChessMove(myPosition, newPosition.copy(), null));
+            newPosition.updatePosition(1, 1);
+        }
+        // Check down and to the left
+        newPosition.updatePosition(myPosition);
+        newPosition.updatePosition(-1, -1);
+        while(this.validMove(newPosition)) {
+            moves.add(new ChessMove(myPosition, newPosition.copy(), null));
+            newPosition.updatePosition(-1, -1);
+        }
+        // Check down and to the right
+        newPosition.updatePosition(myPosition);
+        newPosition.updatePosition(-1, 1);
+        while(this.validMove(newPosition)) {
+            moves.add(new ChessMove(myPosition, newPosition.copy(), null));
+            newPosition.updatePosition(-1, 1);
+        }
+        return moves;
+    }
 }
--- shared/src/main/java/chess/PieceMovesCalculator.java
+++ shared/src/main/java/chess/PieceMovesCalculator.java
@@ -4,4 +4,17 @@ import java.util.Collection;
 
 abstract class PieceMovesCalculator {
     public abstract Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition);
+
+    public boolean validMove(ChessPosition position) {
+        // Check if position is on the board
+        if (position.getRow() < 1 || position.getRow() > 8) {
+            return false;
+        }
+        if (position.getColumn() < 1 || position.getColumn() > 8) {
+            return false;
+        }
+        // Check if position is occupied by another piece of your team
+        // Check if position is occupied by another piece of opponent's team
+        return true;
+    }
 }

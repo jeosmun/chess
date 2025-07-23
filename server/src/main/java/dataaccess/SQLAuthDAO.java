ce8b62ef4eedd465ac15c734264b24036d13c159
Jacob Edward Osmun <osmunjacob@gmail.com>
2025-07-22 21:14:39 -0600

    Implemented SQL AuthDAO. Passes preliminary tests.

--- server/src/main/java/dataaccess/SQLAuthDAO.java
+++ server/src/main/java/dataaccess/SQLAuthDAO.java
@@ -3,6 +3,7 @@ package dataaccess;
 import model.AuthData;
 
 import java.sql.SQLException;
+import java.util.UUID;
 
 public class SQLAuthDAO implements AuthDAO{
 
@@ -26,16 +27,57 @@ public class SQLAuthDAO implements AuthDAO{
 
     @Override
     public AuthData createAuth(String username) throws DataAccessException {
-        return null;
+        String authToken = UUID.randomUUID().toString();
+        String insertAuth = "INSERT INTO auths (username, authtoken) VALUES (?, ?)";
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var insertStatement = conn.prepareStatement(insertAuth)) {
+                insertStatement.setString(1, username);
+                insertStatement.setString(2, authToken);
+                insertStatement.executeUpdate();
+            }
+        }
+        catch (SQLException e) {
+            throw new DataAccessException(e.getMessage());
+        }
+        return new AuthData(authToken, username);
     }
 
     @Override
     public AuthData getAuth(String authToken) throws DataAccessException {
-        return null;
+        String selectAuth = "SELECT username, authtoken FROM auths WHERE authtoken=?";
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var selectStatement = conn.prepareStatement(selectAuth)) {
+                selectStatement.setString(1, authToken);
+                try (var rs = selectStatement.executeQuery()) {
+                    if (!rs.next()) {
+                        return null;
+                    }
+                    String resultUsername = rs.getString(1);
+                    String resultAuthToken = rs.getString(2);
+                    return new AuthData(resultAuthToken, resultUsername);
+                }
+            }
+        }
+        catch (SQLException ex) {
+            throw new DataAccessException(ex.getMessage());
+        }
     }
 
     @Override
     public void deleteAuth(AuthData authData) throws DataAccessException {
-
+        System.out.println(authData.authToken());
+        String deleteAuth = "DELETE FROM auths WHERE authtoken=?";
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var deleteStatement = conn.prepareStatement(deleteAuth)) {
+                deleteStatement.setString(1, authData.authToken());
+                int affectedRows = deleteStatement.executeUpdate();
+                if (affectedRows == 0 || affectedRows > 1) {
+                    throw new DataAccessException(String.format("%d rows deleted from auth DB", affectedRows));
+                }
+            }
+        }
+        catch (SQLException ex) {
+            throw new DataAccessException(ex.getMessage());
+        }
     }
 }

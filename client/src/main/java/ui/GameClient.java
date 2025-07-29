0b01725c6ce6d58d768494b479a941517a13bb2f
Jacob Edward Osmun <osmunjacob@gmail.com>
2025-07-29 06:28:32 -0600

    Add help text to GameClient, add functionality to be able to exit the ingame REPL.

--- client/src/main/java/ui/GameClient.java
+++ client/src/main/java/ui/GameClient.java
@@ -2,6 +2,12 @@ package ui;
 
 import server.ServerFacade;
 
+import java.util.Arrays;
+
+import static ui.EscapeSequences.RESET_TEXT_COLOR;
+import static ui.EscapeSequences.SET_TEXT_COLOR_RED;
+import static ui.State.*;
+
 public class GameClient {
     public final ServerFacade server;
     public final Repl repl;
@@ -9,5 +15,34 @@ public class GameClient {
     public GameClient(ServerFacade server, Repl repl) {
         this.server = server;
         this.repl = repl;
+    }
+
+    public String eval(String input) {
+        try {
+            var tokens = input.toLowerCase().split(" ");
+            var cmd = (tokens.length > 0) ? tokens[0] : "help";
+            var params = Arrays.copyOfRange(tokens, 1, tokens.length);
+            return switch (cmd) {
+                case "exit" -> exit();
+                default -> help();
+            };
+        }
+        catch (Exception ex) {
+            return ex.getMessage();
+        }
+    }
+
+    public String exit() {
+        repl.setState(SIGNEDIN);
+        return "Exiting game menu";
+    }
+
+    public String help() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(SET_TEXT_COLOR_RED);
+        stringBuilder.append("exit");
+        stringBuilder.append(RESET_TEXT_COLOR);
+        stringBuilder.append(" - to exit game menu");
+        return stringBuilder.toString();
     }
 }

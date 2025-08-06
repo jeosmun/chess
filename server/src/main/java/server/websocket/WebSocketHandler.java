43030f667a303ca58a04e12717e868ce0146f234
Jacob Edward Osmun <osmunjacob@gmail.com>
2025-08-05 21:45:16 -0600

    Implementing connect in WebSocketHandler. ConnectCommand extends UserGameCommand, added toString() method to ServerMessage class.

--- server/src/main/java/server/websocket/WebSocketHandler.java
+++ server/src/main/java/server/websocket/WebSocketHandler.java
@@ -1,11 +1,15 @@
 package server.websocket;
 
 import com.google.gson.Gson;
+import dataaccess.DataAccessException;
 import org.eclipse.jetty.websocket.api.Session;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
 import org.eclipse.jetty.websocket.api.annotations.WebSocket;
 import service.GameService;
 import websocket.commands.*;
+import websocket.messages.*;
+
+import java.io.IOException;
 
 @WebSocket
 public class WebSocketHandler {
@@ -17,29 +21,45 @@ public class WebSocketHandler {
     }
 
     @OnWebSocketMessage
-    public void onMessage(Session session, String message) {
+    public void onMessage(Session session, String message) throws IOException, DataAccessException {
         UserGameCommand command = new Gson().fromJson(message, UserGameCommand.class);
         switch (command.getCommandType()) {
-            case CONNECT -> connect((ConnectCommand) command);
-            case MAKE_MOVE -> makeMove((MoveCommand) command);
-            case LEAVE -> leave((LeaveCommand) command);
-            case RESIGN -> resign((ResignCommand) command);
+            case CONNECT -> connect((ConnectCommand) command, session);
+            case MAKE_MOVE -> makeMove((MoveCommand) command, session);
+            case LEAVE -> leave((LeaveCommand) command, session);
+            case RESIGN -> resign((ResignCommand) command, session);
         }
     }
 
-    public void connect(ConnectCommand command) {
+    public void connect(ConnectCommand command, Session session) throws IOException, DataAccessException {
+        int gameID = command.getGameID();
+        ConnectCommand.UserType userType = command.getUserType();
+        String rootToken = command.getAuthToken();
+        Connection connection = new Connection(rootToken, session);
+        connectionManager.add(gameID, connection, userType);
+        // Send a load game back to the root user
+        connection.send(new Gson().toJson(new LoadGameMessage(gameService.getGameData(gameID))));
+        // Send a broadcast message to everyone else
+        String playerType = null;
+        switch (userType) {
+            case WHITE_PLAYER -> playerType = "white";
+            case BLACK_PLAYER -> playerType = "black";
+            case OBSERVER -> playerType = "observer";
+        }
+        String text = String.format("%s has joined the game as %s", command.getUsername(), playerType);
+        NotificationMessage message = new NotificationMessage(text);
+        connectionManager.broadcast(gameID, rootToken, message);
+    }
+
+    public void makeMove(MoveCommand command, Session session) {
 
     }
 
-    public void makeMove(MoveCommand command) {
+    public void leave(LeaveCommand command, Session session) {
 
     }
 
-    public void leave(LeaveCommand command) {
-
-    }
-
-    public void resign(ResignCommand command) {
+    public void resign(ResignCommand command, Session session) {
 
     }
 }
--- shared/src/main/java/websocket/commands/ConnectCommand.java
+++ shared/src/main/java/websocket/commands/ConnectCommand.java
@@ -1,7 +1,26 @@
 package websocket.commands;
 
 public class ConnectCommand extends UserGameCommand {
-    public ConnectCommand(String authToken, Integer gameID) {
+    public enum UserType {
+        WHITE_PLAYER,
+        BLACK_PLAYER,
+        OBSERVER
+    }
+
+    private final UserType userType;
+    private final String username;
+
+    public ConnectCommand(String authToken, Integer gameID, UserType userType, String username) {
         super(CommandType.CONNECT, authToken, gameID);
+        this.userType = userType;
+        this.username = username;
+    }
+
+    public UserType getUserType() {
+        return userType;
+    }
+
+    public String getUsername() {
+        return username;
     }
 }
